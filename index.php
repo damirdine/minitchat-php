0d49c1f29befbe58ccbae1178e82fd390d100d58
damirdine <damirdine@outlook.fr>
2022-05-30 16:52:47 +0200

    tchat message layout

--- index.php
+++ index.php
@@ -1,6 +1,73 @@
-<?php 
-    include_once('header.php');
-    include_once('login.php')
+<?php
+include_once('header.php');
+//include_once('login.php')
 ?>
+<section id="chat" class="container overflow-auto bg-light mt-5">
+    <div class="message p-4">
+        <picture>
+            <source srcset="..." type="image/svg+xml">
+            <img src="..." class="img-fluid img-thumbnail" alt="photo profil">
+        </picture>
 
-<?php include_once('footer.php')?>
+        <div class="message-content">
+            Lorem ipsum dolor sit amet.
+        </div>
+    </div>
+    <hr>
+    <div class="message p-4">
+        <picture>
+            <source srcset="..." type="image/svg+xml">
+            <img src="..." class="img-fluid img-thumbnail" alt="photo profil">
+        </picture>
+
+        <div class="message-content">
+            Lorem ipsum dolor sit amet.
+        </div>
+    </div>
+    <hr>
+    <div class="message p-4">
+        <picture>
+            <source srcset="..." type="image/svg+xml">
+            <img src="..." class="img-fluid img-thumbnail" alt="photo profil">
+        </picture>
+
+        <div class="message-content">
+            Lorem ipsum dolor sit amet.
+        </div>
+    </div>
+    <hr>
+    <div class="message p-4">
+        <picture>
+            <source srcset="..." type="image/svg+xml">
+            <img src="..." class="img-fluid img-thumbnail" alt="photo profil">
+        </picture>
+
+        <div class="message-content">
+            Lorem ipsum dolor sit amet.
+        </div>
+    </div>
+    <hr>
+    <div class="message p-4">
+        <picture>
+            <source srcset="..." type="image/svg+xml">
+            <img src="..." class="img-fluid img-thumbnail" alt="photo profil">
+        </picture>
+
+        <div class="message-content">
+            Lorem ipsum dolor sit amet.
+        </div>
+    </div>
+    <hr>
+    <div class="message p-4">
+        <picture>
+            <source srcset="..." type="image/svg+xml">
+            <img src="..." class="img-fluid img-thumbnail" alt="photo profil">
+        </picture>
+
+        <div class="message-content">
+            Lorem ipsum dolor sit amet.
+        </div>
+    </div>
+    <hr>
+</section>
+<?php include_once('footer.php') ?>
