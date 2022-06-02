f34b3edbe28a68b04949060415079f852e313cf5
damirdine <damirdine@outlook.fr>
2022-06-02 15:40:10 +0200

    prod DB ✅

--- config.php
+++ config.php
@@ -1,8 +1,18 @@
 <?php 
-$DB_HOST = 'localhost';
-$DB_NAME = 'minitchat';
-$DB_USER = 'root';
-$DB_PASSWORD = "";
+//----------------local DB----------
+//$DB_HOST = 'localhost';
+//$DB_NAME = 'minitchat';
+//$DB_USER = 'root';
+//$DB_PASSWORD = "";
+
+//----------------prod DB----------
+$DB_HOST = 'spryrr1myu6oalwl.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+$DB_NAME = 'qigmuxhxcds0o51i';
+$DB_USER = 'h5gcfr5oy6dl2glm';
+$DB_PASSWORD = "id5r160qahugwmbb";
+
+
+
 
 try{
     $db = new PDO(
