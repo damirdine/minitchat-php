e65c782821b77e025f642b80de90b354a0d790e0
damirdine <damirdine@outlook.fr>
2022-06-08 09:55:25 +0200

    date and link update

--- footer.php
+++ footer.php
@@ -2,8 +2,8 @@
 
   <footer class="bg-light text-center text-lg-start mt-auto">
     <div class="text-center p-3">
-      © 2021 Copyright:
-      <a class="text-dark" href="https://openclassrooms.com/">OpenClassrooms</a>
+      © 2022 Copyright:
+      <a class="text-dark" href="https://damirdine.github.io/">damirdine</a>
     </div>
   </footer>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
